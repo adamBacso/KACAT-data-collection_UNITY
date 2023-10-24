3795ac645a54d5fcaa33ca60500a80739e2de9a9
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-10-24 09:40:55 +0200

    create unity project with basic structure

---
+++ Assets/Scripts/gps.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gps : MonoBehaviour
+{
+    [SerializeField] private double latitude;
+    [SerializeField] private double longitude;
+    [SerializeField] private double altitude;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# KACAT-Unity
+ 
