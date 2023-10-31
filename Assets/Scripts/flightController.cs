98e1278b868f769f53ddf7cb28f2734253d0b82d
adamBacso <47544237+adamBacso@users.noreply.github.com>
2023-10-31 07:08:02 +0100

    slice comms sentence and determine the correct way to manipulate the data

--- Assets/Scripts/flightController.cs
+++ Assets/Scripts/flightController.cs
@@ -1,21 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class flightController : MonoBehaviour
 {
+    [SerializeReference] private rfComms comms;
+
     [Header("Physical Properties")]
     [SerializeField] private float mass = 0.3f;
 
-    [Header("Components")]
-    [SerializeReference] private gps gps;
+    [Header("GPS")]
+    //private float latitudeChange = 111319.9f;
 
+
+    private string incoming;
+     
     private void FixedUpdate()
     {
-        if (gps != null)
+        AnalyseSentence();
+    }
+
+    private void AnalyseSentence()
+    {
+        if (comms != null)
         {
-            Debug.Log(gps.WriteSentence());
+            incoming = comms.GetCommsSentence();
         }
+        string[] dataByComponents = incoming.Split('$');
+
+        foreach (string component in dataByComponents)
+        {
+            string[] dataPoints = component.Split(',');
+
+            if (dataPoints[0] == "GPGGA")
+                ProcessGPSData(dataPoints);
+        }
+    }
+
+    private void ProcessGPSData(string[] dataPoints)
+    {
+        Vector3 coordPosition = new Vector3();
+        coordPosition.x = float.Parse(dataPoints[2]); //latitude
+        coordPosition.y = float.Parse(dataPoints[3]); //altitude
+        coordPosition.z = float.Parse(dataPoints[4]); //longitude
     }
 
     public float GetMass()
